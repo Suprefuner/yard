067f52ac52b28292c946423ecd5911499569d9e5
suprefuner <suprefuner@gmail.com>
2023-03-11 13:36:12 +0800

    change baseURL in customFetch

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -12,13 +11,7 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      "/api/v1/": {
-        target: "https://yard-api.onrender.com/",
-        changeOrigin: true,
-      },
+      "/api/v1/": "http://localhost:5000/",
     },
-    // proxy: {
-    //   "/api/v1/": "http://localhost:5000/",
-    // },
   },
 })
