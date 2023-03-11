3052f217104233caaed235877111a7aee03c28e5
suprefuner <suprefuner@gmail.com>
2023-03-11 20:37:26 +0800

    turn off redux dev tool

--- src/store.js
+++ src/store.js
@@ -46,5 +46,5 @@ export const store = configureStore({
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
-  // devTools: process.env.NODE_ENV !== "production",
+  devTools: process.env.NODE_ENV !== "production",
 })
