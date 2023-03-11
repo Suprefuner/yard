2277890abce378502d0ba6932ac9200818320655
suprefuner <suprefuner@gmail.com>
2023-03-11 13:48:31 +0800

    change some setting

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const customFetch = axios.create({
   // SINCE SET UP PROXY SO DOESN'T HAVE TO USE FULL URL (e.g http://localhost:5000)
   // baseURL: "/api/v1/",
-  baseURL: "https://yard-api.onrender.com/api/v1",
+  baseURL: "https://yard-api.onrender.com/api/v1/",
 })
 
 export default customFetch
