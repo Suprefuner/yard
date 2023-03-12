b0e20e2d8e21c1d3bda437cbff1263ba460c6376
suprefuner <suprefuner@gmail.com>
2023-03-12 11:03:30 +0800

    fix some bug

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,8 +3,8 @@ import axios from "axios"
 const customFetch = axios.create({
   // SINCE SET UP PROXY SO DOESN'T HAVE TO USE FULL URL (e.g http://localhost:5000)
   // baseURL: "/api/v1/",
-  // withCredentials: true,
   baseURL: "https://yard-api.onrender.com/api/v1/",
+  withCredentials: true,
 })
 
 export default customFetch
